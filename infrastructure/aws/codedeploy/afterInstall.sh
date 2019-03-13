3aefaccbdab62154d552831663868a99e5bf4720
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2019-03-13 12:41:56 -0700

    update

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,4 +4,4 @@ sudo systemctl stop tomcat.service
 
 sudo rm -rf /var/lib/tomcat/webapps/*
 
-sudo chown tomcat:tomcat /var/lib/tomcat/webapps/webApp-0.0.1-SNAPSHOT.jar
+#sudo chown tomcat:tomcat /var/lib/tomcat/webapps/webApp-0.0.1-SNAPSHOT.jar
