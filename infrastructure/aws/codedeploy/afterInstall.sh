ae03433ab19e1d993bb358157ebbd1b4a68f1319
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2019-03-14 15:59:26 -0700

    update

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,4 +4,4 @@ cd /home/centos/webapp
 sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x webApp-0.0.1-SNAPSHOT.jar
 source /etc/profile.d/envvariable.sh
-nohup java -jar webApp-0.0.1-SNAPSHOT.jar 2> /home/centos/webapp_error
+nohup java -jar webApp-0.0.1-SNAPSHOT.jar > /home/centos/webapp_error 2>&1	
