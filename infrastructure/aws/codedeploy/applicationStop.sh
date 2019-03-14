6086c159101fb775cb88ee7b2db54c01c679b0ed
hemalgadhiya <36687060+hemalgadhiya@users.noreply.github.com>
2019-03-13 19:32:14 -0700

    Update applicationStop.sh

--- infrastructure/aws/codedeploy/applicationStop.sh
+++ infrastructure/aws/codedeploy/applicationStop.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 # stop tomcat service
-assignment5
-sudo rm -rf /var/lib/tomcat/webapps/*
-sudo service tomcat stop
-
-
-sudo service tomcat stop
-assignment5
+sudo rm -rf /opt/tomcat/webapps/*
+sudo systemctl stop tomcat
