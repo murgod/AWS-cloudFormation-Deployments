5ffec99ba11c26e9510d9c6db11a072dbe90246d
hemalgadhiya <36687060+hemalgadhiya@users.noreply.github.com>
2019-03-13 19:31:14 -0700

    Update afterInstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,11 +2,4 @@
 
 sudo systemctl stop tomcat.service
 
-#sudo rm -rf /var/lib/tomcat/webapps/*
-
-sudo chown tomcat:tomcat /var/lib/tomcat/webapps/webApp-0.0.1-SNAPSHOT.war
-
-sudo rm -rf *
-
-sudo chown tomcat:tomcat /var/lib/tomcat/webapps/ROOT.war
-assignment5
+sudo chown tomcat:tomcat /opt/tomcat/webapps/webApp-0.0.1-SNAPSHOT.war
