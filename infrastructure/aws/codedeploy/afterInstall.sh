d4c7921c3a529012b8967dcc9e05b13224cb4700
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2019-03-14 17:04:40 -0700

    update

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -5,4 +5,4 @@ sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x webApp-0.0.1-SNAPSHOT.jar
 source /etc/profile.d/envvariable.sh
 kill -9 $(ps -ef|grep webApp | grep -v grep | awk '{print$2}') 
-nohup java -jar webApp-0.0.1-SNAPSHOT.jar > /home/centos/output.txt	
+nohup java -jar webApp-0.0.1-SNAPSHOT.jar > /home/centos/output.txt &	
