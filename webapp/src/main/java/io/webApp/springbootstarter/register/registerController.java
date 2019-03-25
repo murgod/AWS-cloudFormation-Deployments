323b106e2875ab2ad68b027eb9c29c23b2781a1f
murgod <murgod.a@husky.neu.edu>
2019-03-25 00:52:15 -0400

    adjusting spaces

--- webapp/src/main/java/io/webApp/springbootstarter/register/registerController.java
+++ webapp/src/main/java/io/webApp/springbootstarter/register/registerController.java
@@ -489,7 +489,7 @@ public class registerController {
         logger.info( "Reset Email: " + resetEmail );
       
         //String topicArn = env.getProperty("sns.arn");
-        String topicArn = "arn:aws:sns:us-east-1:247355540530:ForgotPassword";
+        String topicArn="arn:aws:sns:us-east-1:247355540530:ForgotPassword";
         PublishRequest publishRequest = new PublishRequest(topicArn, userDetails.getEmail());
         PublishResult publishResult = snsClient.publish(publishRequest);
         logger.info( "SNS Publish Result: " + publishResult );
