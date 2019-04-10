fbcc5012a6179c6b52af9ed6f3014cfd8c41cc5e
Satish Kumar Anbalagan <anbalagan.s@husky.neu.edu>
2019-04-10 11:21:57 -0700

    File storage exception changes

--- webapp/src/main/java/io/webApp/springbootstarter/exception/FileStorageException.java
+++ webapp/src/main/java/io/webApp/springbootstarter/exception/FileStorageException.java
@@ -3,17 +3,34 @@ package io.webApp.springbootstarter.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * File storage exception class to throw and print errors, inherits
+ * RuntimeException
+ * 
+ * @author satishkumaranbalagan
+ *
+ */
 public class FileStorageException extends RuntimeException {
 	private final static Logger logger = LoggerFactory.getLogger(FileStorageException.class);
 
-    public FileStorageException(String message) {
-        super(message);
-        logger.error(message);
-    }
+	/**
+	 * Log/print the File storage exception
+	 * 
+	 * @param message in String
+	 */
+	public FileStorageException(String message) {
+		super(message);
+		logger.error(message);
+	}
 
-    public FileStorageException(String message, Throwable cause) {
-        super(message, cause);
-    	logger.error(message, cause);
-    }
+	/**
+	 * Throws and Logs/prints the File storage exception
+	 * 
+	 * @param message in String
+	 * @param cause   Throwable object exception
+	 */
+	public FileStorageException(String message, Throwable cause) {
+		super(message, cause);
+		logger.error(message, cause);
+	}
 }
-
