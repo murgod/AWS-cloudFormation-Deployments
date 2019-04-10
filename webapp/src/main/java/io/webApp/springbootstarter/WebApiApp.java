2939c055eeaabcff74d0dd199f4b1ca517707d79
Satish Kumar Anbalagan <anbalagan.s@husky.neu.edu>
2019-04-10 10:02:40 -0700

    WebApp changes

--- webapp/src/main/java/io/webApp/springbootstarter/WebApiApp.java
+++ webapp/src/main/java/io/webApp/springbootstarter/WebApiApp.java
@@ -9,11 +9,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import io.webApp.springbootstarter.fileStorage.FileStorageProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    FileStorageProperties.class
-})
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class WebApiApp {
 	private final static Logger logger = LoggerFactory.getLogger(WebApiApp.class);
+
+	/**
+	 * Web App Main method and starting point 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		logger.info("\n###############Application running##################\n");
